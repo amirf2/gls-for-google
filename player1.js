b9faf936bb48bbb006f7c7ae52ad952976a3c5e3
amirf2 <40579045+amirf2@users.noreply.github.com>
2020-03-23 19:34:16 +0200

    v2

---
+++ player1.js
@@ -0,0 +1,12 @@
+(function() {
+  // Load the script
+  console.log("amir");
+  var script = document.createElement("SCRIPT");
+  script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+  script.type = "text/javascript";
+  script.onload = function() {
+    var $ = window.jQuery;
+    // Use $ here...
+  };
+  document.getElementsByTagName("head")[0].appendChild(script);
+})();
